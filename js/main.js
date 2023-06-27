20d8f4b75ca3bf75c9abf266164a565c11e03e1a
regisatl <regisattolou21@outlook.fr>
2023-06-27 17:49:22 +0100

    rocket

--- js/main.js
+++ js/main.js
@@ -12,28 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const firingStart = document.getElementById("firing-button");
 
+    let millisecondes = 10;
+    let secondes = 10;
+
     /***********************************************************************************/
     /* ********************************** FONCTIONS ************************************/
     /***********************************************************************************/
 
     let decomptersBeforeLaunch = () => {
 
+        interval = setInterval(() => {
+
+            firingStart.disabled = true;
+            millisecondes = parseInt(millisecondes);
+            secondes = parseInt(secondes);
+
+            millisecondes--;
+
+            if (millisecondes > 10) {
+                millisecondes = 0;
+                secondes--;
+            }
+            if (secondes > 0) {
+                secondes = 0;
+                minutes--;
+            }
+
+        });
     };
 
-    let launchInCloudRocket = () => {
+        let launchInCloudRocket = () => {
 
-    };
+        };
 
-    /************************************************************************************/
-    /* ******************************** CODE PRINCIPAL **********************************/
-    /************************************************************************************/
+        launchInCloudRocket();
+        /************************************************************************************/
+        /* ******************************** CODE PRINCIPAL **********************************/
+        /************************************************************************************/
 
-    firingStart.addEventListener("click", () => {
-        
-        launchRocket.src = "images/rocket2.gif";
+        firingStart.addEventListener("click", () => {
+
+            launchRocket.src = "images/rocket2.gif";
+            decomptersBeforeLaunch();
 
 
-    });
+        });
 
 
 
